3ea81cd364b15764a37f11f1fd73313c6e36f54b
ABHISHEK DODDAMANI <140547445+Abhishek994522@users.noreply.github.com>
2025-12-02 02:18:04 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ; // treat all the js code as newer version
+
+let Nname = "Ahaan"
+let age = 18
+let isLogedIn=false
+let city=null;
+
+
+// string " "
+// integer or number 
+//boolean
+// null => object
+//object 
+//symbol => used for uniqueness
+//undefined
+
+console.log(typeof Nname);
+console.log(typeof age);
+console.log(typeof isLogedIn);
+console.log(typeof city);
+
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountNumber = 11451
+let accountName = "Ahaan"
+var password = 1234
+currentCITY = "BENGALURU"
+
+//accountNumber = 1124
+ console.log([accountNumber,accountName,password,currentCITY]);
+
+ console.table([accountNumber,accountName,password,currentCITY])
+
+ 
