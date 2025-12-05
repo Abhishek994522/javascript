914424c7ed09c8c86aa527e613e1a549cd1c1baf
ABHISHEK DODDAMANI <140547445+Abhishek994522@users.noreply.github.com>
2025-12-05 03:18:26 +0000

    commit

---
+++ 01basics/04_objects.js
@@ -0,0 +1,47 @@
+// singleton
+
+// objects --  there are two ways to create a object
+// 1) object literals
+// 2) object constructor
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const myuser = {
+    name : "abhishek",
+    age : 23,
+    email : "abhishek@email.com",
+    isLoggedIn : false,
+    [mySym] : "mykey1" // use [] for symbols
+
+}
+// console.log(myuser.email);
+// console.log(myuser["email"])
+
+// console.log(myuser[mySym]);
+
+// // Object.freeze(myuser)
+// myuser.email = "nagraj@gmail.com"
+
+// console.log(myuser);
+
+
+myuser.greeting = function()
+{
+    console.log("Hello welcome to the 21st century");
+    
+}
+
+console.log(myuser.greeting);
+console.log(myuser.greeting());
+
+myuser.greeting2 = function()
+{
+    console.log(`hello , ${this.name}`);
+    
+}
+
+console.log(myuser.greeting2());
+console.log(myuser.name);
+
